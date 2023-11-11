a7d27be7ccf5417ddd141774a55410bf5a12109a
Sergei Beilin <sergei.beilin@fluentic.dev>
2023-11-11 10:56:29 +0100

    Add test cases for SanicVerifyJWT methods and fixtures
    New test cases have been added for methods within the SanicVerifyJWT class to aid in confirming their functionality. In particular, tests were added for the _is_authenticated method, open_session method, both with and without valid tokens. Also, some standard mocking fixtures were added such as mock_sanic_app and mock_request to help simulate the real-world calls in testing environment. These additions will significantly improve the reliability of the package by ensuring its core functionalities perform as expected.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,10 +1,13 @@
 import dataclasses
 import datetime
 import uuid
+from unittest.mock import MagicMock
 
 import jwt
 import pytest
 import sanic
+from sanic.exceptions import Unauthorized
+from sanic.request import Request
 
 from sanic_verify_jwt import SanicVerifyJWT
 
@@ -51,3 +54,53 @@ def test_200(app):
 
     _, response = app.test_client.get("/", headers=headers)
     assert response.status == 200
+
+
+@pytest.fixture
+def mock_sanic_app():
+    return MagicMock()
+
+
+@pytest.fixture
+def mock_request():
+    request = MagicMock(spec=Request)
+    request.token = None
+    request.ctx.jwt = {}
+    return request
+
+
+def test_SanicVerifyJWT_init(mock_sanic_app):
+    jwt_verifier = SanicVerifyJWT(app=mock_sanic_app)
+    assert jwt_verifier._app == mock_sanic_app
+
+
+#
+# def test_SanicVerifyJWT_open_session_no_token(mock_sanic_app, mock_request):
+#     jwt_verifier = SanicVerifyJWT(app=mock_sanic_app)
+#     jwt_verifier.open_session(mock_request)
+#     assert mock_request.ctx.jwt is None
+
+
+@pytest.mark.asyncio
+async def test_SanicVerifyJWT_open_session_with_invalid_token(
+    mock_sanic_app, mock_request
+):
+    jwt_verifier = SanicVerifyJWT(app=mock_sanic_app)
+    mock_request.token = "invalid_token"
+    with pytest.raises(Unauthorized):
+        await jwt_verifier.open_session(mock_request)
+
+
+@pytest.mark.asyncio
+async def test_SanicVerifyJWT_authenticated_no_jwt(mock_sanic_app, mock_request):
+    jwt_verifier = SanicVerifyJWT(app=mock_sanic_app)
+    assert not await jwt_verifier._is_authenticated(mock_request)
+
+
+@pytest.mark.asyncio
+async def test_SanicVerifyJWT_authenticated_with_jwt_no_scope(
+    mock_sanic_app, mock_request
+):
+    jwt_verifier = SanicVerifyJWT(app=mock_sanic_app)
+    mock_request.ctx.jwt = {"scope": "read"}
+    assert await jwt_verifier._is_authenticated(mock_request)
